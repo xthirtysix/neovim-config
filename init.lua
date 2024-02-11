7f83760c8ede0335f9297fb4309759818dbb05e3
Alexey Bezdetko <ggalliosha@gmail.com>
2024-02-11 22:08:32 +0300

    feat: moved theme settings into a separate file

--- init.lua
+++ init.lua
@@ -18,46 +18,18 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "rebelot/kanagawa.nvim" },
-    {
-        "nvim-telescope/telescope.nvim", tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" }
-    }
-}
 local opts = {}
 
-require("lazy").setup(plugins, opts)
-
--- Default options:
-require('kanagawa').setup({
-    compile = false,             -- enable compiling the colorscheme
-    undercurl = true,            -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true},
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = false,         -- do not set background color
-    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-    colors = {                   -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-    },
-    overrides = function(colors) -- add/modify highlights
-        return {}
-    end,
-    theme = "wave",              -- Load "wave" theme when 'background' option is not set
-    background = {               -- map the value of 'background' option to a theme
-        dark = "wave",           -- try "dragon" !
-        light = "lotus"
-    },
-})
-
--- setup must be called before loading
-vim.cmd("colorscheme kanagawa")
+require("lazy").setup("plugins")
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+local ts_configs = require("nvim-treesitter.configs")
+
+ts_configs.setup({
+    ensure_installed = { "lua", "go", "javascript", "html", "css" },
+    highlight = { enable = true },
+    indent = { enable = true },  
+})
