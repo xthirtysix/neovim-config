5eb2f576fc0d6405a33ca9c4ed12103ae1814b35
Alexey Bezdetko <ggalliosha@gmail.com>
2024-02-11 21:39:22 +0300

    feat: install telescope

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,7 +19,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { "rebelot/kanagawa.nvim" }
+    { "rebelot/kanagawa.nvim" },
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    }
 }
 local opts = {}
 
@@ -51,3 +57,7 @@ require('kanagawa').setup({
 
 -- setup must be called before loading
 vim.cmd("colorscheme kanagawa")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
