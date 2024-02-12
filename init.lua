4effbf8ebbaa0291ad3d72a8c6e0fa0faa6134a4
Alexey Bezdetko <ggalliosha@gmail.com>
2024-02-12 22:55:13 +0300

    chore: applied stylua formatting

--- init.lua
+++ init.lua
@@ -1,34 +1,33 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
         lazypath,
-    })
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 local options = {
-    signcolumn      = "yes",
-    tabstop         = 4,
-    softtabstop     = 4,
-    shiftwidth      = 4,
-    expandtab       = true,
-    number          = true,
-    relativenumber  = true,
-    list            = true,
-    listchars       = {
-        tab = "<- ",
-        trail = "~",
-        extends = ">",
-        precedes = "<",
-        space = "·"
+    signcolumn = 'yes',
+    tabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    number = true,
+    relativenumber = true,
+    list = true,
+    listchars = {
+        tab = '<- ',
+        trail = '~',
+        extends = '>',
+        precedes = '<',
+        space = '·',
     },
 }
 
@@ -36,4 +35,4 @@ for index, value in pairs(options) do
     vim.opt[index] = value
 end
 
-require("lazy").setup("plugins")
+require('lazy').setup 'plugins'
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,52 +1,46 @@
 return {
     {
-        "williamboman/mason.nvim",
+        'williamboman/mason.nvim',
         config = function()
-            require("mason").setup()
-        end
+            require('mason').setup()
+        end,
     },
     {
-        "williamboman/mason-lspconfig.nvim",
+        'williamboman/mason-lspconfig.nvim',
         config = function()
-            require("mason-lspconfig").setup({
+            require('mason-lspconfig').setup {
                 ensure_installed = {
-                    "lua_ls",
-                    "tsserver",
-                    "gopls"
+                    'lua_ls',
+                    'tsserver',
+                    'gopls',
                 },
-                auto_install = true
-            })
-        end
+                auto_install = true,
+            }
+        end,
     },
     {
-        "neovim/nvim-lspconfig",
+        'neovim/nvim-lspconfig',
         config = function()
-            local lspconfig = require("lspconfig")
+            local lspconfig = require 'lspconfig'
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
-            lspconfig.gopls.setup({})
+            lspconfig.lua_ls.setup {}
+            lspconfig.tsserver.setup {}
+            lspconfig.gopls.setup {}
 
+            vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, { desc = 'Go to definition' })
+            vim.keymap.set('n', '<leader>lD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
             vim.keymap.set(
-                "n", "<leader>ld",
-                vim.lsp.buf.definition,
-                { desc = "Go to definition" }
-            )
-            vim.keymap.set(
-                "n", "<leader>lD",
-                vim.lsp.buf.declaration,
-                { desc = "Go to declaration" }
-            )
-            vim.keymap.set(
-                {"n"}, "<leader>ca",
+                { 'n' },
+                '<leader>ca',
                 vim.lsp.buf.code_action,
-                { desc = "Display modal window with code action" }
+                { desc = 'Display modal window with code action' }
             )
             vim.keymap.set(
-                "n", "<leader>li",
+                'n',
+                '<leader>li',
                 vim.lsp.buf.hover,
-                { desc = "Display info about keyword under the cursor" }
+                { desc = 'Display info about keyword under the cursor' }
             )
-        end
-    }
+        end,
+    },
 }
