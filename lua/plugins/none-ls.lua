cd0ec00460fb408a9a3dc005ee6ee9149845a643
Alexey Bezdetko <ggalliosha@gmail.com>
2024-02-14 11:48:43 +0300

    feat: applied apex formatting, disabled cspell

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,19 +3,27 @@ return {
     dependencies = { 'davidmh/cspell.nvim' },
     config = function()
         local null_ls = require 'null-ls'
-        local cspell = require 'cspell'
+        --local cspell = require 'cspell'
 
         null_ls.setup {
             sources = {
                 -- JS/TS
-                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.prettier.with {
+                    filetypes = { 'apex' },
+                    extra_args = { '--plugin=prettier-plugin-apex', '--write' },
+                },
+                null_ls.builtins.diagnostics.pmd.with({
+                    filetypes = {"apex"},
+                    args = {"check", "--dir", "$ROOT", "--format", "json"},
+                    extra_args = {"--rulesets", "apex_ruleset.xml"}
+                }),
                 -- Lua
                 null_ls.builtins.formatting.stylua,
                 -- Go
                 -- Common
-                null_ls.builtins.diagnostics.cspell,
+                null_ls.builtins.diagnostics.typos,
                 null_ls.builtins.completion.spell,
-                cspell.code_actions,
+                --cspell.code_actions,
             },
         }
 
